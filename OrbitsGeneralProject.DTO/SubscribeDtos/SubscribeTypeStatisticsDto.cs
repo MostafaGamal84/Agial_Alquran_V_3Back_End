01ed2920498d5912be3e09e41efd3fe0256e4cd0
Mostafa Gamal <98846791+MostafaGamal84@users.noreply.github.com>
2025-09-17 16:43:47 +0300

    Add subscribe type statistics endpoint

---
+++ OrbitsGeneralProject.DTO/SubscribeDtos/SubscribeTypeStatisticsDto.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace Orbits.GeneralProject.DTO.SubscribeDtos
+{
+    public class SubscribeTypeStatisticsDto
+    {
+        public List<string> Labels { get; set; } = new();
+
+        public List<int> Series { get; set; } = new();
+
+        public List<SubscribeTypeStatisticItemDto> Items { get; set; } = new();
+
+        public int TotalSubscriptions { get; set; }
+
+        public int UniqueSubscribers { get; set; }
+    }
+
+    public class SubscribeTypeStatisticItemDto
+    {
+        public int? SubscribeTypeId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int SubscriptionCount { get; set; }
+
+        public int UniqueStudentCount { get; set; }
+
+        public decimal Percentage { get; set; }
+    }
+}
