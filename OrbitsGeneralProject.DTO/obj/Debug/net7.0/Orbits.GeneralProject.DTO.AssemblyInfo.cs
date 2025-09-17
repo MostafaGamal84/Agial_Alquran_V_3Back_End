1d1d91c8022a2698f20b66e529a5257a6ba27079
MostafaGamal84 <mostafa.ddamoni22@gmail.com>
2025-09-17 15:46:24 +0300

    0

--- OrbitsGeneralProject.DTO/obj/Debug/net7.0/Orbits.GeneralProject.DTO.AssemblyInfo.cs
+++ OrbitsGeneralProject.DTO/obj/Debug/net7.0/Orbits.GeneralProject.DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orbits.GeneralProject.DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d098e1d5c8fca72bc0643849d71a8d7ba6e252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8869f58adb227e7c55279b2843e3edae78cfd986")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orbits.GeneralProject.DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orbits.GeneralProject.DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
