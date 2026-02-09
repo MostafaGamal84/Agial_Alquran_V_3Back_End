cf008af2b372333d6d4120043b8b4f8502b1ef68
Mostafa Gamal <mostafa.ddamoni22@gmail.com>
2026-02-09 16:38:31 +0300

    قليبل

--- OrbitsGeneralProject.Core/Entities/OrbitsContext.cs
+++ OrbitsGeneralProject.Core/Entities/OrbitsContext.cs
@@ -45,7 +45,10 @@ namespace Orbits.GeneralProject.Core.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("workstation id=ajyalDBcopy.mssql.somee.com;packet size=4096;user id=ajyal_alquran_SQLLogin_1;pwd=uxb1px7683;data source=ajyalDBcopy.mssql.somee.com;persist security info=False;initial catalog=ajyalDBcopy;TrustServerCertificate=True");
+                //optionsBuilder.UseSqlServer("workstation id=ajyalDBcopy.mssql.somee.com;packet size=4096;user id=ajyal_alquran_SQLLogin_1;pwd=uxb1px7683;data source=ajyalDBcopy.mssql.somee.com;persist security info=False;initial catalog=ajyalDBcopy;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer(
+                          "Data Source=Mostafa-Gamal;Initial Catalog=ajyalDBcopy;Integrated Security=True;TrustServerCertificate=True"
+                      );
             }
         }
 
